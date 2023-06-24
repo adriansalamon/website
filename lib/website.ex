b73bcf0a39ccaf254a2dff442db8ffe9d14fc322
Adrian Salamon <adr.salamon@gmail.com>
2023-06-24 16:08:35 +0200

    start

--- lib/website.ex
+++ lib/website.ex
@@ -1,18 +1,64 @@
 defmodule Website do
-  @moduledoc """
-  Documentation for `Website`.
-  """
+  use Phoenix.Component
+  import Phoenix.HTML
 
-  @doc """
-  Hello world.
+  def post(assigns) do
+    ~H"""
+    <.layout>
+      <%= raw(@post.body) %>
+    </.layout>
+    """
+  end
 
-  ## Examples
+  def index(assigns) do
+    ~H"""
+    <.layout>
+      <h1>Adrians website</h1>
+      <ul>
+        <li :for={post <- @posts}>
+          <a href={post.path}>
+            <%= post.title %>
+          </a>
+        </li>
+      </ul>
+    </.layout>
+    """
+  end
 
-      iex> Website.hello()
-      :world
+  def layout(assigns) do
+    ~H"""
+    <html>
+      <body>
+        <%= render_slot(@inner_block) %>
+      </body>
+    </html>
+    """
+  end
 
-  """
-  def hello do
-    :world
+  @output_dir "./output"
+
+  def build() do
+    File.mkdir_p!(@output_dir)
+    posts = Website.Blog.all_posts()
+
+    render_file("index.html", index(%{posts: posts}))
+
+    for post <- posts do
+      dir = Path.dirname(post.path)
+
+      if dir != "." do
+        File.mkdir_p!(Path.join([@output_dir, dir]))
+      end
+
+      render_file(post.path, post(%{post: post}))
+    end
+
+    :ok
+  end
+
+  def render_file(path, rendered) do
+    safe = Phoenix.HTML.Safe.to_iodata(rendered)
+    output = Path.join([@output_dir, path])
+    File.write!(output, safe)
   end
 end
